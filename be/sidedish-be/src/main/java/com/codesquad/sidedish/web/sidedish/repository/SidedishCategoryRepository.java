fa044edb0f4a6776963e24d8dbaef657846bd457
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-04-29 22:32:40 +0900

    [ADD] : 반찬 카테고리별로 객체를 가져오는 메서드 추가
    반찬의 카테고리와 베스트상품인지를 확인하여 List혹은 SidedishCategory를 반환하도록 하는 메서드를 추가하였다.

--- be/sidedish-be/src/main/java/com/codesquad/sidedish/web/sidedish/repository/SidedishCategoryRepository.java
+++ be/sidedish-be/src/main/java/com/codesquad/sidedish/web/sidedish/repository/SidedishCategoryRepository.java
@@ -3,5 +3,9 @@ package com.codesquad.sidedish.web.sidedish.repository;
 import com.codesquad.sidedish.web.sidedish.domain.SidedishCategory;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SidedishCategoryRepository extends CrudRepository<SidedishCategory, Long> {
+    SidedishCategory findByNameIs(String name);
+    List<SidedishCategory> findByIsBestTrue();
 }
